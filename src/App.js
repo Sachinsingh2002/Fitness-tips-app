a5486ea5440a748eef76d64048a8c65423afc592
Sachin Singh <108183252+Sachinsingh2002@users.noreply.github.com>
2024-09-08 12:47:49 +0530

    files added

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import FitnessTips from './FitnessTips';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <FitnessTips />
+    </div>
+  );
+}
+
+export default App;
